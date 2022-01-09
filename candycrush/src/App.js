0e3b384e13dff910cc1eef21c7056f67fc2cdc2a
Gustavo <gustavokkg2@hotmail.com>
2022-01-09 13:55:23 -0300

    Cria board 8x8

--- candycrush/src/App.js
+++ candycrush/src/App.js
@@ -1,10 +1,32 @@
-const width = 8
-const candyColors = ['blue', 'green', 'orange', 'purple', 'red', 'yellow']
+import { useEffect, useState } from "react";
+const width = 8;
+const candyColors = ['blue', 'green', 'orange', 'purple', 'red', 'yellow'];
 
 
-function App() {
+const App = () => {
+  
+  const [currentColorArrangement, setCurrentColorArrangement] = useState([])
+
+  const createBoard = () => {
+    const randomColorArrangement = []
+    for(let i = 0; i < width * width; i++) {
+      const randomColor = candyColors[Math.floor(Math.random() * candyColors.length)]
+      randomColorArrangement.push(randomColor)
+    }
+    setCurrentColorArrangement(randomColorArrangement)
+  }
+
+  useEffect(() => {
+    createBoard()
+  }, [])
+
+
   return (
-    <div>
+    <div className="app">
+      <div className="game">
+        {currentColorArrangement.map((candyColor, index) => (
+        <img key={index} style={{backgroundColor: candyColor}} alt={candyColor}/>))}
+        </div>
     </div>
   );
 }
