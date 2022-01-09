86c2b3d793307f6edf1b74622423315e0a42b16e
Gustavo <gustavokkg2@hotmail.com>
2022-01-09 14:45:07 -0300

    Cria checagem de match entre frutas

--- candycrush/src/App.js
+++ candycrush/src/App.js
@@ -7,6 +7,64 @@ const App = () => {
   
   const [currentColorArrangement, setCurrentColorArrangement] = useState([])
 
+  //Checa match de colunas de 4
+  const checkForColumnOfFour = () => {
+    for (let i = 0; i < 39; i++) {
+      const columnOfFour = [i, i + width, i + width * 2, i + width * 3]
+      const decidedColor = currentColorArrangement[i]
+
+      if ( columnOfFour.every(square => currentColorArrangement[square] === decidedColor)) {
+        columnOfFour.forEach(square => currentColorArrangement[square] = '')
+      }
+    }
+  }
+
+  //Checa match de colunas de 3
+  const checkForColumnOfThree = () => {
+    for (let i = 0; i < 47; i++) {
+      const columnOfThree = [i, i + width, i + width * 2]
+      const decidedColor = currentColorArrangement[i]
+
+      if ( columnOfThree.every(square => currentColorArrangement[square] === decidedColor)) {
+        columnOfThree.forEach(square => currentColorArrangement[square] = '')
+      }
+    }
+  }
+  //checa match de linhas de 4
+  const checkForRowOfFour = () => {
+    for (let i = 0; i < 64; i++) {
+      const rowOfFour = [i, i + 1, i + 2, i + 3]
+      const decidedColor = currentColorArrangement[i]
+      const notValid = [5, 6, 7, 13, 14, 15, 21, 22, 23, 29, 30, 31, 37, 38, 39, 45, 46, 47, 53, 54, 55, 62, 63, 64]
+
+      if (notValid.includes(i)) continue
+
+      if ( rowOfFour.every(square => currentColorArrangement[square] === decidedColor)) {
+        rowOfFour.forEach(square => currentColorArrangement[square] = '')
+      }
+    }
+  }
+
+  //checa match de linhas de 3
+  const checkForRowOfThree = () => {
+    for (let i = 0; i < 64; i++) {
+      const rowOfThree = [i, i + 1, i + 2]
+      const decidedColor = currentColorArrangement[i]
+      const notValid = [6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55, 63, 64]
+
+      if (notValid.includes(i)) continue
+
+      if ( rowOfThree.every(square => currentColorArrangement[square] === decidedColor)) {
+        rowOfThree.forEach(square => currentColorArrangement[square] = '')
+      }
+    }
+  }
+
+
+
+
+
+  //cria tabuleiro de itens
   const createBoard = () => {
     const randomColorArrangement = []
     for(let i = 0; i < width * width; i++) {
@@ -19,6 +77,20 @@ const App = () => {
   useEffect(() => {
     createBoard()
   }, [])
+
+  //Lida com o timing de checagem
+  useEffect(() => {
+    const timer = setInterval(() => {
+      checkForColumnOfFour()
+      checkForColumnOfThree()
+      checkForRowOfFour()
+      checkForRowOfThree()
+      setCurrentColorArrangement([...currentColorArrangement])
+    }, 100)
+    return () => clearInterval(timer)
+
+  }, [checkForColumnOfFour, checkForColumnOfThree,checkForRowOfFour, checkForRowOfThree, setCurrentColorArrangement])
+
 
 
   return (
